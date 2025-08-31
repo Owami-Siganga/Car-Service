cf07425c833eb1afa2d8dff8e37eba097f55be49
Aneeqah Talaaboedien <219099405@mycput.ac.za>
2025-09-01 01:52:10 +0200

    backend

---
+++ CarServices/src/test/java/za/ac/cput/carservices/CarServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package za.ac.cput.carservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
